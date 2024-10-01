35bccca7dc6592eed9f6aadb90d84e7d050a3940
Fadl <chaos@efqr.dev>
2024-10-01 11:00:21 +0100

    move $post/objects/bundle -> $post/objects #8

--- obstracts/server/views.py
+++ obstracts/server/views.py
@@ -437,7 +437,7 @@ class PostView(viewsets.ViewSet):
         summary="Get STIX Objects for a specific Post",
         description="This endpoint will return all objects extracted for a post. If you want more flexibility to filter the objects or search for STIX objects across different Posts, use the Get Object endpoints.",
     )
-    @decorators.action(detail=True, methods=["GET"], url_path='objects/bundle')
+    @decorators.action(detail=True, methods=["GET"])
     def objects(self, request, feed_id=None, post_id=None):
         return ArangoDBHelper(settings.VIEW_NAME, request).get_post_objects(post_id, feed_id)
 
