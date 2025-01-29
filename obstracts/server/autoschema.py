730df865ab7ca1275f774bc72fcfb2937842224c
David Greenwood <himynamesdave@users.noreply.github.com>
2025-01-29 10:39:05 +0000

    updating swagger (#127)
    * updating swagger
    
    * Update requirements.txt

--- obstracts/server/autoschema.py
+++ obstracts/server/autoschema.py
@@ -22,12 +22,13 @@ class OverrideDjangoFilterExtension(DjangoFilterExtension):
 
 class ObstractsAutoSchema(CustomAutoSchema):
     url_path_params: list[OpenApiParameter] = [
-        OpenApiParameter('feed_id', type=uuid.UUID, location=OpenApiParameter.PATH, description="You can search and retrieve a Feed ID for a blog using the Get Feeds endpoint."),
-        OpenApiParameter('post_id', type=uuid.UUID, location=OpenApiParameter.PATH, description="You can search and retrieve a Post ID for a blog using the Get Posts for a Feed endpoint."),
-        OpenApiParameter('job_id', type=uuid.UUID, location=OpenApiParameter.PATH, description="You can search and retrieve a Job ID for a blog using the Get Jobs endpoint."),
-        OpenApiParameter('object_id', type=str, location=OpenApiParameter.PATH, description="This is the STIX ID of an objects, e.g. `threat-actor--dfaa8d77-07e2-4e28-b2c8-92e9f7b04428`. You can search and retrieve a STIX object ID using the Get Objects SDO/SCO/SRO endpoints."),
-        OpenApiParameter('profile_id', type=str, location=OpenApiParameter.PATH, description="You can search and retrieve a Profile ID for a Profile you have created using the Get Profiles endpoint."),
+        OpenApiParameter('feed_id', type=uuid.UUID, location=OpenApiParameter.PATH, description="You can search and retrieve a Feed ID for a blog using the Get Feeds endpoint. The value is always a UUIDv4, e.g. `16c1ec28-aee1-470d-84a2-a56314511e1c`"),
+        OpenApiParameter('post_id', type=uuid.UUID, location=OpenApiParameter.PATH, description="You can search and retrieve a Post ID for a blog using the Get Posts for a Feed endpoint. The value is always a UUIDv4, e.g. `ad343f60-4432-4b0d-9cca-2c3069eeef14`"),
+        OpenApiParameter('job_id', type=uuid.UUID, location=OpenApiParameter.PATH, description="You can search and retrieve a Job ID for a blog using the Get Jobs endpoint. The value is always a UUIDv4, e.g. `2ece3fa5-e04b-457b-97cf-aef6efba8580"),
+        OpenApiParameter('object_id', type=str, location=OpenApiParameter.PATH, description="This is the STIX ID of an object, e.g. `threat-actor--dfaa8d77-07e2-4e28-b2c8-92e9f7b04428`, `ipv4-addr--3f604e4b-d889-4f8b-9e8e-93d34fb12f7a`. You can search and retrieve a STIX object ID using the Get Objects SDO/SCO/SRO endpoints."),
+        OpenApiParameter('profile_id', type=str, location=OpenApiParameter.PATH, description="You can search and retrieve a Profile ID for a Profile you have created using the Get Profiles endpoint. The value is always a UUIDv4, e.g. `85c4d787-0906-4d3c-a580-be980c4ebf86"),
         OpenApiParameter('extractor_id', type=str, location=OpenApiParameter.PATH, description="You can search and retrieve an Extractor ID using the Get Extractors endpoint. An example ID is; `lookup_mitre_cwe`"),
+        OpenApiParameter('report_id', type=str, location=OpenApiParameter.PATH, description="This is the STIX ID of a report object, e.g. `report--0c16301f-7b2f-45c7-b4c5-c35b464e9696`. You can search and retrieve a STIX Report object ID using the Get SDO objects endpoints."),
     ]
     def get_override_parameters(self):
         params = super().get_override_parameters()
