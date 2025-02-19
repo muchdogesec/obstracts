d29722adbb42c31ab70daad170edc4c810fee3f8
David G <himynamesdave@users.noreply.github.com>
2025-02-19 16:02:41 +0000

    Revert "implement content-check (#135)"
    This reverts commit 632fb0340afe51d31d9150d83aee873a4ec57d47.

--- obstracts/server/models.py
+++ obstracts/server/models.py
@@ -78,9 +78,6 @@ class File(models.Model):
     markdown_file = models.FileField(upload_to=upload_to_func, null=True)
     summary = models.CharField(max_length=65535, null=True)
     profile = models.ForeignKey(Profile, on_delete=models.PROTECT, default=None, null=True)
-    ai_describes_incident = models.BooleanField(default=None, null=True)
-    ai_incident_summary = models.CharField(default=None, max_length=65535, null=True)
-    ai_incident_classification = models.CharField(default=None, max_length=256, null=True)
 
     def __str__(self) -> str:
         return f'File(feed_id={self.feed_id}, post_id={self.post_id})'
@@ -140,7 +137,6 @@ class Job(models.Model):
     failed_processes = models.IntegerField(default=0)
     feed = models.ForeignKey(FeedProfile, on_delete=models.CASCADE, null=True)
     profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
-    ai_content_check_variable = models.CharField(default=None, null=True)
     
     @property
     def feed_id(self):
