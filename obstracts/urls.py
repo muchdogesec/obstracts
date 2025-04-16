5d3dede1adefdac74cf062fe59330ceceff84b5d
Chaos Pjeles <chaos@efqr.dev>
2025-04-16 16:18:04 +0100

    fix potential leakage issue in schema generation (#182)

--- obstracts/urls.py
+++ obstracts/urls.py
@@ -65,7 +65,7 @@ urlpatterns = [
     path(f'api/{API_VERSION}/', include(obj_router.urls)),
     path('admin/', admin.site.urls),
     # YOUR PATTERNS
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/', views.SchemaViewCached.as_view(), name='schema'),
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
