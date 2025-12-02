062203ce499ad847a18b79ef94798aa1c6f50957
Fadl <chaos@efqr.dev>
2025-12-02 16:29:55 +0100

    allow empty feed in job response

--- obstracts/server/serializers.py
+++ obstracts/server/serializers.py
@@ -11,7 +11,7 @@ from django.utils.translation import gettext_lazy as _
 
 class ObstractsJobSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField()
-    feed_id = serializers.PrimaryKeyRelatedField(read_only=True, source="feed")
+    feed_id = serializers.PrimaryKeyRelatedField(read_only=True, source="feed", required=False, allow_null=True)
     profile_id = serializers.PrimaryKeyRelatedField(read_only=True, source="profile", required=False, allow_null=True)
 
     class Meta:
