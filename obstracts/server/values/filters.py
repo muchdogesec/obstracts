e07915b5ce1e632ad7f1d7f76c4841776da0ea87
lullah <chaos@efqr.dev>
2026-04-01 15:30:46 +0100

    use new method for filtering #890

--- obstracts/server/values/filters.py
+++ obstracts/server/values/filters.py
@@ -2,42 +2,21 @@
 from django.db.models import JSONField, Lookup
 from django.db.models import Func, CharField
 
-
-
-@JSONField.register_lookup
-class JSONValueTrigram(Lookup):
-    """
-    Custom lookup for searching within JSONB values using the jsonb_values_concat function with trigram matching.
-
-    Usage:
-        ObjectValue.objects.filter(values__jsonb_trg='search_term')
-
-    This will search for 'search_term' within all values in the JSONB field using trigram similarity (%).
-    """
-
-    lookup_name = "jsonb_trg"
-
-    def as_sql(self, compiler, connection):
-        lhs, lhs_params = self.process_lhs(compiler, connection)
-        rhs, rhs_params = self.process_rhs(compiler, connection)
-
-        sql = f"jsonb_values_concat({lhs}) %% %s"
-        return sql, lhs_params + rhs_params
-
-
 @JSONField.register_lookup
 class JSONValueContains(Lookup):
     """
-    Custom lookup for substring matching within concatenated JSONB values using the jsonb_values_concat function.
+    Custom lookup for substring matching within concatenated JSONB values
+    using the jsonb_values_concat function.
 
     Usage:
         ObjectValue.objects.filter(values__jsonb_vcontains='search_term')
 
-    This will search for 'search_term' within the concatenated values in the JSONB field using ILIKE.
+    This will search for 'search_term' within the concatenated JSONB values
+    using ILIKE.
     """
 
     lookup_name = "jsonb_vcontains"
-    
+
     def process_rhs(self, compiler, connection):
         rhs, params = super().process_rhs(compiler, connection)
         params[0] = f"%{params[0]}%"
@@ -67,16 +46,12 @@ class JSONValueExact(Lookup):
     def as_sql(self, compiler, connection):
         lhs, lhs_params = self.process_lhs(compiler, connection)
         rhs, rhs_params = self.process_rhs(compiler, connection)
-
-        # Use jsonb_each_text to iterate over key-value pairs and check if any value matches exactly
-        sql = f"EXISTS (SELECT 1 FROM jsonb_each_text({lhs}) WHERE value = %s)"
+        sql = f"jsonb_values_list({lhs}) @> ARRAY[LOWER(%s)]"
         return sql, lhs_params + rhs_params
 
-class NormalizeDict(Func):
+class DictFirstValue(Func):
     """
-    Returns the value for the first key (alphabetically) from a JSONB object.
+    Returns the first value from a JSONB array.
     """
-
-    template = "(SELECT kv.value FROM jsonb_each_text(%(expressions)s) AS kv ORDER BY kv.key LIMIT 1)"
+    function = "jsonb_first_value"
     output_field = CharField()
-
