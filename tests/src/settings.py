e58efc7c3ea347f27ae68cb4cf1aa6e28d5dc690
lullah <chaos@efqr.dev>
2026-06-03 14:49:25 +0100

    fix test fails / use memory cache for tests

--- tests/src/settings.py
+++ tests/src/settings.py
@@ -3,6 +3,7 @@ from obstracts.settings import *
 ARANGODB_DATABASE = "obstracts_test"
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-cache-name",
     }
 }
